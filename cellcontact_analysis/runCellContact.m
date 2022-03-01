8c4405c6113e20bff0e3a99c5cc30dbe11dcc5c5
hkim240 <hbkim20005@gmail.com>
2022-03-01 11:12:54 +0100

    Update cellcontact_analysis

--- cellcontact_analysis/runCellContact.m
+++ cellcontact_analysis/runCellContact.m
@@ -22,13 +22,13 @@
 % [INPUT]
 %
 % Embryo:
-% 'embryo_matchMarker_plus_selected.csv'
-% 'mouseEmbryo_Slideseq_RCTD_top2000_plus.mat'
+% 'input/embryo/embryo_matchMarker_plus_selected.csv'
+% 'input/embryo/mouseEmbryo_Slideseq_RCTD_top2000_plus.mat'
 % 'input/embryo/celltype1+celltype2.mat'
 %
 % Hippocampus:
-% 'hippocampus_matchMarker_plus_selected.csv'
-% 'mouseHippocampus_Slideseq_RCTD_top2000_plus.mat'
+% 'input/hippocampus/hippocampus_matchMarker_plus_selected.csv'
+% 'input/hippocampus/mouseHippocampus_Slideseq_RCTD_top2000_plus.mat'
 % 'input/hippocampus/celltype1+celltype2.mat'
 %
 % The details of the input files are described below the command lines.
@@ -39,17 +39,19 @@
 %
 % [OUTPUT]
 %
-% Cell contact-dependent genes: DEGs, fdr, log-ratio, p-value
-% 'celltype1+celltype2_stat_null.mat' (from the null model)
-% 'celltype1+celltype2_stat_cellContact.mat' (from the comparison between 
-% heterotypic and homotypic beads)
+% Differentially expressed genes: DEGs, fdr, log-ratio, p-value
+% (i) DEGs identified from the null model
+% 'celltype1+celltype2_stat_null.mat'
+% (ii) DEGs identified from the comparison between heterotypic and homotypic beads
+% 'celltype1+celltype2_stat_cellContact.mat'
 % 
-% Info of DEG expression: bead barcode, log-value, z-value
+% Cell contact-dependent genes: bead barcode, log-value, z-value
 % 'celltype1+celltype2_gene.txt'
 % 'celltype1+celltype1_gene.txt'
 % 'celltype2+celltype2_gene.txt'
 %
-% Heatmaps:
+% Heatmaps: expression of cell contact-dependent genes & cell type markers
+% in homotypic and heterotypic beads
 % 'celltype1+celltype2.pdf'
 % 
 % The output files above are saved in "output/celltype1+celltype2/"
@@ -60,7 +62,7 @@ tic % measuring execution time: start point
 
 %%%% Adding the informaton of cell type markers 
 % Importing data
-matchMarker = importdata('hippocampus_matchMarker_plus_selected.csv'); % embryo/hippocampus
+matchMarker = importdata('input/embryo/embryo_matchMarker_plus_selected.csv'); % choose embryo or hippocampus
 
 % Data: 'embryo_matchMarker_plus_selected.csv' where each row means a gene
 % It says which gene is which cell type marker among genes from 1st to 2089th.
@@ -90,86 +92,110 @@ end
 
 
 %%%% Conducting cell contact-dependent gene expression analysis
-files = dir('input/hippocampus/*.mat'); % embryo/hippocampus
+files = dir('input/embryo/*.mat'); % choose embryo or hippocampus
 
 for k = 1:length(files)
     
     baseFileName = files(k).name;
-    name = regexprep(baseFileName,'.mat','');
     
-    center_celltype = name;
-       
-    % Importing data
-    load 'mouseHippocampus_Slideseq_RCTD_top2000_plus.mat' % Embryo/Hippocampus
+    if ~strcmp(baseFileName,'mouseHippocampus_Slideseq_RCTD_top2000_plus.mat') & ~strcmp(baseFileName,'mouseEmbryo_Slideseq_RCTD_top2000_plus.mat')
+
+        name = regexprep(baseFileName,'.mat','');
+        
+        center_celltype = name; % heterotypic pair
+           
+        % Importing data
+        load 'input/embryo/mouseEmbryo_Slideseq_RCTD_top2000_plus.mat' % choose Embryo or Hippocampus
+        
+        % Data: 'mouseEmbryo_Slideseq_RCTD_top2000_plus.mat'
+        % 'cell_id' - 42362 spot IDs
+        % 'gene_name' - top 2000 plus cell type markers (2089 genes)
+        % 'log_data' - log-normalized data (2089 genes x 42362 spots)
+        
+        % Data:'mouseHippocampus_Slideseq_RCTD_top2000_plus.mat'
+        % 'cell_id' - 41344 spot IDs
+        % 'gene_name' - top 2000 plus cell type markers (2034 genes)
+        % 'log_data' - log-normalized data (2034 genes x 41344 spots)
     
-    % Data: 'mouseEmbryo_Slideseq_RCTD_top2000_plus.mat'
-    % 'cell_id' - 42362 spot IDs
-    % 'gene_name' - top 2000 plus cell type markers (2089 genes)
-    % 'log_data' - log-normalized data (2089 genes x 42362 spots)
+        fullFileName = fullfile(files(k).folder, baseFileName);
+        load(fullFileName);
     
-    % Data:'mouseHippocampus_Slideseq_RCTD_top2000_plus.mat'
-    % 'cell_id' - 41344 spot IDs
-    % 'gene_name' - top 2000 plus cell type markers (2034 genes)
-    % 'log_data' - log-normalized data (2034 genes x 41344 spots)
-
-    fullFileName = fullfile(files(k).folder, baseFileName);
-    load(fullFileName);
-
-    % Data:'celltype1(ct1)+celltype2(ct2).mat'
-    % 'index' - Boolean membership for ct1+ct2, ct1+ct1, ct2+ct2 
-    %           among 42362 spots for embryo/41344 spots for hippocampus
-    %           sum(index) means the total number of ct1+ct2, ct1+ct1, and ct2+ct2 spots
-    % 'neiCombUnique' - three types of pairs: ct1+ct2(1), ct1+ct1(2), ct2+ct2(3)
-    % 'matchComb' - sum(index) spots classified into the three types
-    % 'prop' - cell type proportions of true heterotypic spots (ct1+ct2)
-
-
-    %----------------------------------------------------------------------
-    % STEP1: Converting log-normalized data to z-values 
-    %----------------------------------------------------------------------
-    cell_id_total = cell_id;
-    cell_id = cell_id_total(index);
-
-    log_data_total=log_data;
-    log_data=log_data_total(:,index);
-    log_data_zvalue=(log_data-repmat(mean(log_data,2),1,size(log_data,2)))./repmat(std(log_data')',1,size(log_data,2));
-    log_data_zvalue(isnan(log_data_zvalue))=0;
-
-
-    %----------------------------------------------------------------------
-    % STEP2: Creating the null model of artificial heterotypic spots
-    %----------------------------------------------------------------------
-    seedNumber=1; randSize=100;
-    clusterSelect=unique(matchComb);
-    log_data_artificialHeteroSpots=createNullModel(seedNumber,randSize,prop,matchComb,clusterSelect,log_data);
+        % Data:'celltype1(ct1)+celltype2(ct2).mat'
+        % 'index' - Boolean membership for ct1+ct2, ct1+ct1, ct2+ct2 
+        %           among 42362 spots for embryo/41344 spots for hippocampus
+        %           sum(index) means the total number of ct1+ct2, ct1+ct1, and ct2+ct2 spots
+        % 'neiCombUnique' - three types of pairs: ct1+ct2(1), ct1+ct1(2), ct2+ct2(3)
+        % 'matchComb' - sum(index) spots classified into the three types
+        % 'prop' - cell type proportions of true heterotypic spots (ct1+ct2)
     
-
-    %----------------------------------------------------------------------
-    % STEP3: Finding differentially expressed genes (DEGs)
-    %----------------------------------------------------------------------
-    % Setting cut-off values
-    pCutoff=0.01; pCutoff2=0.001; 
-    lrCutoff=0.7; % for embryo=0.58, for hippocampus=0.7
-
-    % Comparing heterotypic beads with artificial heterotypic beads
-    folderName='output/';  
-    folderName2=[folderName, center_celltype];
-    mkdir(folderName2);
-    [null_DEGs, pvalue_null,fdr_null,logRatio_null]=findNullDEGs(center_celltype,clusterSelect,matchComb,neiCombUnique,log_data,log_data_artificialHeteroSpots,gene_name,pCutoff,pCutoff2,lrCutoff);
-    save([folderName2,'/',name,'_stat_null.mat'],'null_DEGs','pvalue_null','fdr_null','logRatio_null');
-
-    % Comparing heterotypic beads with homotypic beads
-    load([folderName2,'/',name,'_stat_null.mat']);
-    [cellContact_DEGs_IDX,cellContact_DEGs,pvalue1_cellContact,fdr1_cellContact,logRatio1_cellContact,pvalue2_cellContact,fdr2_cellContact,logRatio2_cellContact]=findCellContactDEGs(center_celltype,clusterSelect,matchComb,neiCombUnique,log_data,gene_name,pCutoff,lrCutoff,null_DEGs);
-    save([folderName2,'/',name,'_stat_cellContact.mat'],'cellContact_DEGs_IDX','cellContact_DEGs','pvalue1_cellContact','fdr1_cellContact','logRatio1_cellContact','pvalue2_cellContact','fdr2_cellContact','logRatio2_cellContact');
-
     
-    %----------------------------------------------------------------------
-    % SETP4: Plotting heatmaps 
-    %----------------------------------------------------------------------
-    plotHeatmaps(center_celltype,clusterSelect,matchComb,neiCombUnique,log_data,log_data_zvalue,gene_name,cell_id,cellContact_DEGs_IDX,logRatio1_cellContact,logRatio2_cellContact,matchMarker_var,folderName2);
+        %----------------------------------------------------------------------
+        % STEP1: Converting log-normalized data to z-values 
+        %----------------------------------------------------------------------
+        cell_id_total = cell_id;
+        cell_id = cell_id_total(index);
     
-   
+        log_data_total=log_data;
+        log_data=log_data_total(:,index);
+        log_data_zvalue=(log_data-repmat(mean(log_data,2),1,size(log_data,2)))./repmat(std(log_data')',1,size(log_data,2));
+        log_data_zvalue(isnan(log_data_zvalue))=0;
+    
+    
+        %----------------------------------------------------------------------
+        % STEP2: Creating the null model of artificial heterotypic spots
+        %----------------------------------------------------------------------
+        seedNumber=1; randSize=100;
+        clusterSelect=unique(matchComb);
+        log_data_artificialHeteroSpots=createNullModel(seedNumber,randSize,prop,matchComb,clusterSelect,log_data);
+        
+    
+        %----------------------------------------------------------------------
+        % STEP3: Finding differentially expressed genes (DEGs)
+        %----------------------------------------------------------------------
+        % Setting cut-off values
+        pCutoff=0.01; pCutoff2=0.001; 
+        lrCutoff=0.58; % set log-ratio: for embryo=0.58, for hippocampus=0.7
+    
+        % Comparing heterotypic beads with artificial heterotypic beads
+        folderName='output/';  
+        folderName2=[folderName, center_celltype];
+        [null_DEGs, pvalue_null,fdr_null,logRatio_null]=findNullDEGs(center_celltype,clusterSelect,matchComb,neiCombUnique,log_data,log_data_artificialHeteroSpots,gene_name,pCutoff,pCutoff2,lrCutoff);
+        
+        if size(null_DEGs,1) > 0
+    
+            mkdir(folderName2);
+            save([folderName2,'/',name,'_stat_null.mat'],'null_DEGs','pvalue_null','fdr_null','logRatio_null');
+        
+        end    
+        
+    
+        % Comparing heterotypic beads with homotypic beads
+        if size(null_DEGs,1) > 0
+    
+            load([folderName2,'/',name,'_stat_null.mat']);
+            [cellContact_DEGs_IDX,cellContact_DEGs,pvalue1_cellContact,fdr1_cellContact,logRatio1_cellContact,pvalue2_cellContact,fdr2_cellContact,logRatio2_cellContact]=findCellContactDEGs(center_celltype,clusterSelect,matchComb,neiCombUnique,log_data,gene_name,pCutoff,lrCutoff,null_DEGs);
+            
+            if size(cellContact_DEGs,1) > 0
+    
+                save([folderName2,'/',name,'_stat_cellContact.mat'],'cellContact_DEGs_IDX','cellContact_DEGs','pvalue1_cellContact','fdr1_cellContact','logRatio1_cellContact','pvalue2_cellContact','fdr2_cellContact','logRatio2_cellContact');
+            
+            end
+    
+        end
+        
+    
+        
+        %----------------------------------------------------------------------
+        % SETP4: Plotting heatmaps 
+        %----------------------------------------------------------------------
+        if size(null_DEGs,1) > 0 & size(cellContact_DEGs,1) > 0
+    
+            plotHeatmaps(center_celltype,clusterSelect,matchComb,neiCombUnique,log_data,log_data_zvalue,gene_name,cell_id,cellContact_DEGs_IDX,logRatio1_cellContact,logRatio2_cellContact,matchMarker_var,folderName2);
+        
+        end
+    
+    end
+
 end    
 
 
