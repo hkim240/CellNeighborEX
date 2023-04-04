8dad7f50034672f99e1e562c554cab16d3817401
hkim240 <hbkim20005@gmail.com>
2023-04-04 11:34:03 -0700

    updated

--- cellcontact_analysis/runCellContact.m
+++ cellcontact_analysis/runCellContact.m
@@ -2,7 +2,7 @@
 % CellContact is a statistical framework to detect cell contact-dependent 
 % gene expression. This is a new framework to analyze spatial transcriptomics 
 % (ST) data using spatial beads composed of multiple cell types, especially 
-% for Slide-seq data with 10 µm resolution. 
+% for Slide-seq data with 10 µm resolution.. 
 %
 %
 % [ST DATA]
