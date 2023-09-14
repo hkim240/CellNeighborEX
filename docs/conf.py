b4b5402053269752c1a68eb19a753f710f7734b4
hkim240 <hbkim20005@gmail.com>
2023-09-13 23:28:54 -0700

    update Docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ release = '0.0.5'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
     'nbsphinx',
 	'sphinx_gallery.load_style',
 	'sphinx.ext.imgmath',
